f36e661073ac9c04a3e90d7a1ddb09c352e90dda
Daniel2711 <detaddeo.d@gmail.com>
2020-12-17 16:30:58 +0100

    Todo list con data in input

--- js/app.js
+++ js/app.js
@@ -1,25 +1,48 @@
 var app = new Vue({
     
-    el: "#root",
+    el: "#app",
     
-    data: {
+    data:{
+
+        //nome applicazione
         title_app: 'To do list',
 
-        new_todo: '',
+        // variabili input
+        text_todo: '',
+        date_todo: '',
 
+        //to do list
         todo_list: [
-            'fare la spesa',
-            'stampare i fogli',
-            'andare dal dentista'
+            {
+                date: '12/12/2020',
+                todo: 'fare la spesa'
+            
+            },{
+                date: '23/12/2020',
+                todo: 'biglietti natalizi virtuali'
+            
+            },{
+                date: '21/12/2020',
+                todo: 'imparare vue'
+            }
         ]
     },
 
     methods: {
+        // aggiuge todo alla lista
         addTodo() {
-           this.todo_list.push(this.new_todo);
+
+            let new_todo = {
+                todo: this.text_todo,
+                date: this.date_todo
+            };
+
+           this.todo_list.push(new_todo);
+           
            this.new_todo = '';
         },
 
+        // rimuove al click l'elemento todo dalla lista
         removeTodo(index) {
            this.todo_list.splice(index, 1);
         }
